87ab3b52fff5a6ea1c291dd3f9af00a6efb993b5
Kestrel_omega <40593588+Kestrel-omega@users.noreply.github.com>
2021-05-28 22:34:11 +0900

    Update jiho.cpp

--- jiho.cpp
+++ jiho.cpp
@@ -70,6 +70,63 @@ void displayLCD(float temp, float humid)
   snprintf(str2,"Humid : %f %%", humid);
 }
 
+int readDHT11(int *readTemp, int* readHumid)
+{
+
+  int dt[82] = {0,};
+  //Phase 1
+  digitalWrite(DHT11PIN, 1);
+  pinMode(DHT11PIN, OUTPUT);
+  delay(1);
+  digitalWrite(DHT11PIN, 0);
+  delay(20);
+  pinMode(DHT11PIN, INPUT_PULLUP);
+  /*
+    while(1)
+      if(digitalRead(DHT11PIN)==1) break;
+    while(1)
+      if(digitalRead(DHT11PIN)==0) break;
+  */
+  //Phase 2, 3
+  int cnt = 0;
+  for (cnt = 0; cnt < 41; cnt++)
+  {
+    dt[cnt * 2] = micros();
+    while (1)
+      if (digitalRead(DHT11PIN) == 1) break;
+    dt[cnt * 2] = micros() - dt[cnt * 2];
+
+    dt[cnt * 2 + 1] = micros();
+    while (1)
+      if (digitalRead(DHT11PIN) == 0) break;
+    dt[cnt * 2 + 1] = micros() - dt[cnt * 2 + 1];
+  }
+
+  //Phase 4
+  *readHumid = 0;
+  *readTemp = 0;
+  for (cnt = 2; cnt < 11; cnt++) //Humid
+  {
+    *readHumid = *readHumid << 1;
+    if (dt[cnt * 2 + 1] > 49)
+      *readHumid = *readHumid + 1;
+    else
+      *readHumid = *readHumid + 0;
+
+  }
+  for (cnt = 18; cnt < 26; cnt++) //Temp
+  {
+    *readTemp = *readTemp << 1;
+    if (dt[cnt * 2 + 1] > 49)
+      *readTemp = *readTemp + 1;
+    else
+      *readTemp = *readTemp + 0;
+  }
+  //for(cnt=0;cnt<41;cnt++)
+  //  Serial.printf("cnt:%d, dt[%d, %d]\r\n",cnt,dt[cnt*2],dt[cnt*2+1]);
+  return 1;
+}
+
 // get current weather data from Openweather API
 void getWeather()
 {
