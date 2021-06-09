74b002574ba2a560cf34605437340faf2523dc13
gaeul <56909921+euleey@users.noreply.github.com>
2021-06-09 12:48:52 +0900

    Update gaeul.h

--- gaeul.h
+++ gaeul.h
@@ -6,18 +6,10 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <Servo.h> 
-#include <HX711.h>
 
-#define calibration_factor -7050.0 
-#define DOUT  4 //data Pin
-#define CLK  5 // clk Pin
 #define servoPin 9 //servo Pin
 #define buzzerPin 12//buzz
 
-
-HX711 scale(DOUT, CLK);
-Servo servo; 
-ESP8266WebServer myHttpServer(80);//have to call "myHttpServer.handleClient();" as many as possible in loop()
 
 
 void settingGaeul();
@@ -38,8 +30,7 @@ void buzz(int value);
 //turn on when true, turn off when false
 void servofunc(int angle);
 //servo motor contol as angle
-float getloadcell();
-//return readed weight
+
 
 
 #endif
